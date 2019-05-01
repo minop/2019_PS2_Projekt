997afd42659e1586f0d7c050635f3218d3e3af0a
Jakub <jakub@kovar.co.uk>
2019-05-01 11:59:39 +0200

    Pociatocny stav projektu - wifi a ethernet + uzly

--- projekt.cc
+++ projekt.cc
@@ -1,19 +1,3 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
 #include "ns3/core-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/network-module.h"
@@ -24,24 +8,11 @@
 #include "ns3/yans-wifi-helper.h"
 #include "ns3/ssid.h"
 
-// Default Network Topology
-//
-//   Wifi 10.1.3.0
-//                 AP
-//  *    *    *    *
-//  |    |    |    |    10.1.1.0
-// n5   n6   n7   n0 -------------- n1   n2   n3   n4
-//                   point-to-point  |    |    |    |
-//                                   ================
-//                                     LAN 10.1.2.0
-
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("ThirdScriptExample");
-
-int 
-main (int argc, char *argv[])
-{
+int
+main(int argc, char *argv[]) {
+    /*
   bool verbose = true;
   uint32_t nCsma = 3;
   uint32_t nWifi = 3;
@@ -51,136 +22,143 @@ main (int argc, char *argv[])
   cmd.AddValue ("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
   cmd.AddValue ("nWifi", "Number of wifi STA devices", nWifi);
   cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
-  cmd.AddValue ("tracing", "Enable pcap tracing", tracing);
+  cmd.AddValue ("tracing", "Enable pcap tracing", tracing); 
 
   cmd.Parse (argc,argv);
+     */
 
-  // The underlying restriction of 18 is due to the grid position
-  // allocator's configuration; the grid layout will exceed the
-  // bounding box if more than 18 nodes are provided.
-  if (nWifi > 18)
-    {
-      std::cout << "nWifi should be 18 or less; otherwise grid layout exceeds the bounding box" << std::endl;
-      return 1;
-    }
+    // Server Node
+    Node sn = Node();
+    Ptr<Node> server = Ptr<Node>(&sn);
+    
+    // AP Nodes
+    NodeContainer apNodes;
+    apNodes.Create(20);
+    
+    // UAV Node
+    Node rn = Node();
+    Ptr<Node> robot = Ptr<Node>(&rn);
+    
+    // helper containers to install nodes more easily
+    NodeContainer ethernetNodes;
+    ethernetNodes.Add(server);
+    ethernetNodes.Add(apNodes);
+    
+    NodeContainer wifiNodes;
+    wifiNodes.Add(apNodes);
+    wifiNodes.Add(robot);
 
-  if (verbose)
-    {
-      LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-      LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
-    }
+    
+    // Ethernet connection from APs to server
+    CsmaHelper ethernet;
+    // TODO: ethernet attributes
+    ethernet.SetChannelAttribute("DataRate", StringValue("100Mbps"));
+    ethernet.SetChannelAttribute("Delay", TimeValue(NanoSeconds(6560)));
 
-  NodeContainer p2pNodes;
-  p2pNodes.Create (2);
+    NetDeviceContainer ethernetDevices;
+    ethernetDevices = ethernet.Install(ethernetNodes);
 
-  PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+    // Wifi connection from robot to APs
+    // TODO: wifi attributes
+    YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
+    YansWifiPhyHelper phy = YansWifiPhyHelper::Default();
+    phy.SetChannel(channel.Create());
 
-  NetDeviceContainer p2pDevices;
-  p2pDevices = pointToPoint.Install (p2pNodes);
+    WifiHelper wifi;
+    wifi.SetRemoteStationManager("ns3::AarfWifiManager");
 
-  NodeContainer csmaNodes;
-  csmaNodes.Add (p2pNodes.Get (1));
-  csmaNodes.Create (nCsma);
+    WifiMacHelper mac;
+    Ssid ssid = Ssid("ns-3-ssid");
+    mac.SetType("ns3::StaWifiMac",
+            "Ssid", SsidValue(ssid),
+            "ActiveProbing", BooleanValue(false));
 
-  CsmaHelper csma;
-  csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
-  csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
+    NetDeviceContainer wifiDevices;
+    wifiDevices = wifi.Install(phy, mac, wifiNodes);
 
-  NetDeviceContainer csmaDevices;
-  csmaDevices = csma.Install (csmaNodes);
+    mac.SetType("ns3::ApWifiMac",
+            "Ssid", SsidValue(ssid));
 
-  NodeContainer wifiStaNodes;
-  wifiStaNodes.Create (nWifi);
-  NodeContainer wifiApNode = p2pNodes.Get (0);
+    
+    // AP Mobility
+    MobilityHelper apMobility;
+    // TODO: final physical layout of the nodes
+    apMobility.SetPositionAllocator("ns3::GridPositionAllocator",
+            "MinX", DoubleValue(10.0),
+            "MinY", DoubleValue(10.0),
+            "DeltaX", DoubleValue(20.0),
+            "DeltaY", DoubleValue(20.0),
+            "GridWidth", UintegerValue(5),
+            "LayoutType", StringValue("RowFirst"));
 
-  YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
-  YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
-  phy.SetChannel (channel.Create ());
+    apMobility.Install(apNodes);
 
-  WifiHelper wifi;
-  wifi.SetRemoteStationManager ("ns3::AarfWifiManager");
+    apMobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    apMobility.Install(apNodes);
+    
+    // server Mobility
+    MobilityHelper serverMobility;
+    serverMobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    serverMobility.SetPositionAllocator("ns3::GridPositionAllocator",
+            "MinX", DoubleValue(300.0),
+            "MinY", DoubleValue(75.0));
+    serverMobility.Install(server);
+    
+    // robot Mobility
+    // TODO: some mobility
+    MobilityHelper robotMobility;
+    robotMobility.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
+            "Bounds", RectangleValue(Rectangle(0, 100, 0, 100)));
+    robotMobility.SetPositionAllocator("ns3::GridPositionAllocator",
+            "MinX", DoubleValue(50.0),
+            "MinY", DoubleValue(50.0));
+    robotMobility.Install(robot);
+    
+    
+    // InternetStack
+    InternetStackHelper stack;
+    stack.Install(server);
+    stack.Install(apNodes);
+    stack.Install(robot);
 
-  WifiMacHelper mac;
-  Ssid ssid = Ssid ("ns-3-ssid");
-  mac.SetType ("ns3::StaWifiMac",
-               "Ssid", SsidValue (ssid),
-               "ActiveProbing", BooleanValue (false));
+    
+    // Addresses
+    Ipv4AddressHelper address;
 
-  NetDeviceContainer staDevices;
-  staDevices = wifi.Install (phy, mac, wifiStaNodes);
+    address.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer ethernetInterfaces;
+    ethernetInterfaces = address.Assign(ethernetDevices);
 
-  mac.SetType ("ns3::ApWifiMac",
-               "Ssid", SsidValue (ssid));
+    address.SetBase("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer wifiInterfaces;
+    wifiInterfaces = address.Assign(wifiDevices);
 
-  NetDeviceContainer apDevices;
-  apDevices = wifi.Install (phy, mac, wifiApNode);
 
-  MobilityHelper mobility;
+    // Application
+    // TODO:
+    /*
+    UdpEchoServerHelper echoServer(9);
 
-  mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
-                                 "MinX", DoubleValue (0.0),
-                                 "MinY", DoubleValue (0.0),
-                                 "DeltaX", DoubleValue (5.0),
-                                 "DeltaY", DoubleValue (10.0),
-                                 "GridWidth", UintegerValue (3),
-                                 "LayoutType", StringValue ("RowFirst"));
+    ApplicationContainer serverApps = echoServer.Install(csmaNodes.Get(nCsma));
+    serverApps.Start(Seconds(1.0));
+    serverApps.Stop(Seconds(10.0));
 
-  mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
-                             "Bounds", RectangleValue (Rectangle (-50, 50, -50, 50)));
-  mobility.Install (wifiStaNodes);
+    UdpEchoClientHelper echoClient(csmaInterfaces.GetAddress(nCsma), 9);
+    echoClient.SetAttribute("MaxPackets", UintegerValue(1));
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
 
-  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-  mobility.Install (wifiApNode);
+    ApplicationContainer clientApps =
+            echoClient.Install(wifiStaNodes.Get(nWifi - 1));
+    clientApps.Start(Seconds(2.0));
+    clientApps.Stop(Seconds(10.0));
+    */
+    
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
-  InternetStackHelper stack;
-  stack.Install (csmaNodes);
-  stack.Install (wifiApNode);
-  stack.Install (wifiStaNodes);
+    Simulator::Stop(Seconds(10.0));
 
-  Ipv4AddressHelper address;
-
-  address.SetBase ("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer p2pInterfaces;
-  p2pInterfaces = address.Assign (p2pDevices);
-
-  address.SetBase ("10.1.2.0", "255.255.255.0");
-  Ipv4InterfaceContainer csmaInterfaces;
-  csmaInterfaces = address.Assign (csmaDevices);
-
-  address.SetBase ("10.1.3.0", "255.255.255.0");
-  address.Assign (staDevices);
-  address.Assign (apDevices);
-
-  UdpEchoServerHelper echoServer (9);
-
-  ApplicationContainer serverApps = echoServer.Install (csmaNodes.Get (nCsma));
-  serverApps.Start (Seconds (1.0));
-  serverApps.Stop (Seconds (10.0));
-
-  UdpEchoClientHelper echoClient (csmaInterfaces.GetAddress (nCsma), 9);
-  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
-  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
-
-  ApplicationContainer clientApps = 
-    echoClient.Install (wifiStaNodes.Get (nWifi - 1));
-  clientApps.Start (Seconds (2.0));
-  clientApps.Stop (Seconds (10.0));
-
-  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-
-  Simulator::Stop (Seconds (10.0));
-
-  if (tracing == true)
-    {
-      pointToPoint.EnablePcapAll ("third");
-      phy.EnablePcap ("third", apDevices.Get (0));
-      csma.EnablePcap ("third", csmaDevices.Get (0), true);
-    }
-
-  Simulator::Run ();
-  Simulator::Destroy ();
-  return 0;
+    Simulator::Run();
+    Simulator::Destroy();
+    return 0;
 }
